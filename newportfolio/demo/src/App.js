edb73dd7ff1cd360a7088d4b845ddfb369a2733f
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-07-28 17:36:17 -0400

    building keeper app

--- newportfolio/demo/src/App.js
+++ newportfolio/demo/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Note from './component/Note';
 
 function App() {
   return (
  <div>
- 
+ <Header/>
+ <Note/>
+ <Footer/>
     </div>
   );
 }
---
+++ newportfolio/demo/src/component/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Note = () => {
+  return (
+    <div>
+    <h1>Welcome to my keeper app!</h1>
+    <p>Here you will learn how everything is done behind the scenes</p>
+    </div>
+  )
+}
+
+export default Note
